dc83d9bab4174c4bccc5085c94947063212057ad
Temii-mp3 <ogunleyeamiel@gmail.com>
2025-12-06 15:44:19 -0600

    implement insert function and fixed bug

--- project3.py
+++ project3.py
@@ -20,6 +20,159 @@ def create(filename):
         return False
     
 
+def insert(filename, key, value):
+    try:
+        with open(filename, "rb+") as file:
+            file.seek(0)
+            header = file.read(24)
+            magic_number, root_block_id, next_block_id = struct.unpack(">8sQQ", header)
+
+            if root_block_id == 0:
+                root_block_id = next_block_id
+                next_block_id += 1
+
+                root_node = create_btree_node(root_block_id, 0, [(key, value)])
+                file.seek(root_block_id * 512)
+                file.write(root_node)
+
+                file.seek(8)
+                file.write(struct.pack(">QQ", root_block_id, next_block_id))
+                return True
+
+            root = read_node_at_block(file, root_block_id)
+            
+            if root["num_keys"] == 19:
+                new_root_block_id = next_block_id
+                next_block_id += 1
+                
+                next_block_id, middle_key, middle_value, new_node_block_id = split_node(
+                    file, root_block_id, next_block_id, root_block_id
+                )
+                
+                new_root_keys = [middle_key] + [0] * 18
+                new_root_values = [middle_value] + [0] * 18
+                new_root_children = [root_block_id, new_node_block_id] + [0] * 18
+                
+                node_format = ">QQQ" + "Q" * 19 + "Q" * 19 + "Q" * 20
+                new_root_node = struct.pack(node_format,
+                    new_root_block_id, 0, 1,
+                    *new_root_keys, *new_root_values, *new_root_children)
+                
+                file.seek(new_root_block_id * 512)
+                file.write(new_root_node)
+                
+                update_parent_id(file, root_block_id, new_root_block_id)
+                update_parent_id(file, new_node_block_id, new_root_block_id)
+                
+                file.seek(8)
+                file.write(struct.pack(">QQ", new_root_block_id, next_block_id))
+                
+                root_block_id = new_root_block_id
+            
+            next_block_id = insert_non_full(file, root_block_id, key, value, next_block_id)
+            
+            file.seek(16)
+            file.write(struct.pack(">Q", next_block_id))
+            
+            return True
+    except Exception as e:
+        print(f"Error during insert: {e}")
+        return False
+    
+def insert_non_full(file, block_id, key, value, next_block_id):
+    node = read_node_at_block(file, block_id)
+    
+    if is_leaf(node):
+        keys = node["keys"][:node["num_keys"]]
+        values = node["values"][:node["num_keys"]]
+        
+        insert_pos = 0
+        while insert_pos < len(keys) and keys[insert_pos] < key:
+            insert_pos += 1
+        
+        keys.insert(insert_pos, key)
+        values.insert(insert_pos, value)
+        
+        keys_padded = keys + [0] * (19 - len(keys))
+        values_padded = values + [0] * (19 - len(values))
+        
+        node_format = ">QQQ" + "Q" * 19 + "Q" * 19 + "Q" * 20
+        updated_node = struct.pack(node_format,
+            node["block_id"], node["parent_id"], len(keys),
+            *keys_padded, *values_padded, *node["children"])
+        
+        file.seek(block_id * 512)
+        file.write(updated_node)
+        
+        return next_block_id
+    else:
+        i = 0
+        while i < node["num_keys"] and key > node["keys"][i]:
+            i += 1
+        
+        child_id = node["children"][i]
+        child = read_node_at_block(file, child_id)
+        
+        if child["num_keys"] == 19:
+            next_block_id, middle_key, middle_value, new_child_id = split_node(
+                file, child_id, next_block_id, block_id
+            )
+            	
+            node = read_node_at_block(file, block_id)
+            
+            keys = list(node["keys"][:node["num_keys"]])
+            values = list(node["values"][:node["num_keys"]])
+            children = list(node["children"][:node["num_keys"] + 1])
+            
+            insert_pos = 0
+            while insert_pos < len(keys) and keys[insert_pos] < middle_key:
+                insert_pos += 1
+            
+            keys.insert(insert_pos, middle_key)
+            values.insert(insert_pos, middle_value)
+            children.insert(insert_pos + 1, new_child_id)
+            
+            keys_padded = keys + [0] * (19 - len(keys))
+            values_padded = values + [0] * (19 - len(values))
+            children_padded = children + [0] * (20 - len(children))
+            
+            node_format = ">QQQ" + "Q" * 19 + "Q" * 19 + "Q" * 20
+            updated_node = struct.pack(node_format,
+                node["block_id"], node["parent_id"], len(keys),
+                *keys_padded, *values_padded, *children_padded)
+            
+            file.seek(block_id * 512)
+            file.write(updated_node)
+            
+            if key > middle_key:
+                i += 1
+            
+            child_id = children[i]
+        
+        next_block_id = insert_non_full(file, child_id, key, value, next_block_id)
+        
+        return next_block_id
+
+def read_node_at_block(file, block_id):
+    file.seek(block_id * 512)
+    node_data = file.read(512)
+    return read_btree_node(node_data)
+
+def read_btree_node(data):
+    node_data = data[:488]
+    node_format = ">QQQ" + "Q" * 19 + "Q" * 19 + "Q" * 20
+    unpacked = struct.unpack(node_format, node_data)
+
+    return {
+        "block_id": unpacked[0],
+        "parent_id": unpacked[1],
+        "num_keys": unpacked[2],
+        "keys": list(unpacked[3:22]),
+        "values": list(unpacked[22:41]),
+        "children": list(unpacked[41:61]),
+    }
+
+
 def update_parent_id(file, block_id, new_parent_id):
     file.seek(block_id * 512 + 8)
     file.write(struct.pack(">Q", new_parent_id))
